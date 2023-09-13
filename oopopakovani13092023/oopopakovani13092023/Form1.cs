47921203549a1372910b0c096a7f1c4cf28806cf
Radekkstach <97514874+Radekkstach@users.noreply.github.com>
2023-09-13 11:26:25 +0200

    Add files via upload

---
+++ oopopakovani13092023/oopopakovani13092023/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace oopopakovani13092023
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Zamestnanec radek = new Zamestnanec("ing", "radek stach", 20000, dateTimePicker1.Value);
+            label1.Text = radek.ToString();
+        }
+    }
+}
---
+++ oopopakovani13092023/oopopakovani13092023/Zamestnanec.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oopopakovani13092023
+{
+    class Zamestnanec
+    {
+        private string titul;
+        private string jmeno;
+        private double plat;
+        private DateTime datum;
+        private double hodnoceni = 0;
+        private string pozice;
+        private int odpracovaneHodiny;
+
+        public Zamestnanec(string titul,string jmeno, double plat, DateTime datum)
+        {
+            this.titul = titul;
+            this.jmeno = jmeno;
+            this.plat = plat;
+            this.datum = datum;
+        }
+
+        string Jmeno
+        {
+            get { return jmeno; }
+            set
+            {
+                string[] pomoc = jmeno.Split(' ');
+
+                string jmenicko = pomoc[0];
+                string prijmeni = pomoc[1];
+
+                jmenicko.Replace(jmenicko.First(), Convert.ToChar(jmenicko.ToUpper()));
+                prijmeni.ToUpper();
+
+
+                Titul = jmenicko + " " + prijmeni;
+            }
+        }
+
+        string Titul
+        {
+            get { return titul; }
+            set
+            {
+                titul.Trim();
+                titul.Replace(titul.First(), Convert.ToChar(titul.ToUpper()));
+                titul.Insert(titul.Length, ".");
+
+                Titul = titul;
+            }
+        }
+
+        public double RocniPlat()
+        {
+            double platZaRok = plat * 12;
+            return platZaRok;
+        }
+
+        public int PocetMesicu()
+        {
+            TimeSpan celkemdnu;
+            celkemdnu = DateTime.Now - datum;
+            return celkemdnu.Days / 30;
+        }
+
+        public double CelkovyPlat()
+        {
+            double celkem;
+            celkem = PocetMesicu() * plat;
+            return celkem;
+        }
+
+        public void ZvysOsobni(int zvyseni)
+        {
+            hodnoceni += zvyseni;
+            if(hodnoceni > 10000)
+            {
+                pozice = "Vedouci";
+            }
+        }
+
+        int relaxace = 0;
+
+        public void PridatOdpracovaneHodiny(int x)
+        {
+            if (odpracovaneHodiny >= 100)
+            {
+                if (relaxace >= 76)
+                {
+                    odpracovaneHodiny = 0;
+                    relaxace = 0;
+                }
+            }
+            odpracovaneHodiny += x;
+            
+        }
+
+        public void RelaxujuJakKokot(int x)
+        {
+            relaxace += x;
+        }
+
+        public override string ToString()
+        {
+            return "" + Titul +" "+ Jmeno +" "+ plat + PocetMesicu() + hodnoceni + RocniPlat() + CelkovyPlat() + odpracovaneHodiny + pozice;
+        }
+    }
+}
